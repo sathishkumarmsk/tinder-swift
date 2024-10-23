8e3e5161dcd8b9e67a03c020d54b07061988da22
sathihskuamr M <msksathishzz@gmail.com>
2024-10-23 15:41:17 +0530

    Initial Commit

---
+++ Tinder/TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderApp.swift
+//  Tinder
+//
+//  Created by Sathish on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TinderTests/TinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  TinderTests.swift
+//  TinderTests
+//
+//  Created by Sathish on 23/10/24.
+//
+
+import Testing
+@testable import Tinder
+
+struct TinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
